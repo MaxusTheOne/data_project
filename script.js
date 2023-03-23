0c776627fd3632819ea863be15c26b12b33976da
Markus Bille <43919485+MaxusTheOne@users.noreply.github.com>
2023-03-23 11:46:20 +0100

    ændret til projectet

--- script.js
+++ script.js
@@ -1,38 +1,43 @@
-let harry = {
-  firstName: "Harry",
-  lastName: "Potter",
+"use strict";
+let characterList = document.querySelector("#character_list");
+
+let mrMackey = {
+  name: "Mr. Mackey",
+  nickname: "Fireman Tom",
+  image: (URL = "https://static.wikia.nocookie.net/southpark/images/5/58/Mr_Mackey_New.png/revision/latest?cb=20211125042917"),
+  occupation: "School Counselor",
+  age: 50,
+  voicedBy: "Trey Parker",
+  gender: "Male",
+  religion: "Roman Catholic",
+  catchPhrase: "m'kay",
+  hairColor: "black",
+  schoolGrade: null,
+  episodes: [],
+  appearences: 0, //idk
+  firstAppearence: "S01E09",
 };
-let ron = {
-  firstName: "ron",
-  lastName: "Weasley",
-};
-let hermione = {
-  firstName: "Hermione",
-  lastName: "Granger",
-};
-const chosenOne = harry;
-console.log(harry);
-showStudent(harry);
-function showStudent(student) {
-  document.querySelector("#firstName").textContent = student.firstName;
-  document.querySelector("#middleName").textContent = student.middleName;
-  document.querySelector("#lastName").textContent = student.lastName;
-}
-harry.middleName = "James";
-ron.middleName = "Bilius";
-hermione.middleName = "Jean";
-studentList = document.querySelector("#students");
-function addStudent(student) {
-  let studentHTML = /*HTML*/ `
+addCharacter(mrMackey);
+function addCharacter(character) {
+  let characterHTML = /*HTML*/ `
         <li>
             <img src="images/profile.jpg" alt="">
-            <p class = "firstName">First name: <span>${student.firstName}</span></p>
-            <p class = "middleName">Middle name: <span>${student.middleName}</span></p>
-            <p class = "lastName">Last name: <span>${student.lastName}</span></p>
+            <p class = "name">Name: <span>${character.name}</span></p>
+            <p class = "name">Nickname: <span>${character.nickname}</span></p>
+            <p class = "name">Image: <span>${character.image}</span></p>
+            <p class = "name">Occupation: <span>${character.occupation}</span></p>
+            <p class = "name">Age: <span>${character.age}</span></p>
+            <p class = "name">Voiced by: <span>${character.voicedBy}</span></p>
+            <p class = "name">Gender: <span>${character.gender}</span></p>
+            <p class = "name">Religion: <span>${character.religion}</span></p>
+            <p class = "name">Catch phrase: <span>${character.catchPhrase}</span></p>
+            <p class = "name">Hair color: <span>${character.hairColor}</span></p>
+            <p class = "name">School grade: <span>${character.schoolGrade}</span></p>
+            <p class = "name">Episodes: <span>${character.episodes}</span></p>
+            <p class = "name">Appearences: <span>${character.appearences}</span></p>
+            <p class = "name">First appearence: <span>${character.firstAppearence}</span></p>
+
         </li>
     `;
-  studentList.insertAdjacentHTML("beforeend", studentHTML);
+  characterList.insertAdjacentHTML("beforeend", characterHTML);
 }
-addStudent(harry);
-addStudent(ron);
-addStudent(hermione);
